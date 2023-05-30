4c43f42012cf536edef271eaf3c1596e4f94d2cd
Fábrica de Software CEET <vcortes@sena.edu.co>
2023-05-30 16:01:53 -0500

    feat(Backend): Adicione archivos del proyecto base

---
+++ sena.ceet.adso.WebApplicationWithIdentityMVC003/sena.ceet.adso.WebApplicationWithIdentityMVC003/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace sena.ceet.adso.WebApplicationWithIdentityMVC003.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) 
+            : base(options)
+        {
+            
+        }
+    }
+}
---
+++ sena.ceet.adso.WebApplicationWithIdentityMVC003/sena.ceet.adso.WebApplicationWithIdentityMVC003/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sena.ceet.adso.WebApplicationWithIdentityMVC003.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
